916d65b54621179aad46c24151c33bd641751ac3
Viktor <viktor@farcic.com>
2016-09-16 16:41:35 -0700

    DM Test Swarm services script

--- scripts/dm-test-swarm-services.sh
+++ scripts/dm-test-swarm-services.sh
@@ -82,4 +82,4 @@ while true; do
     fi
 done
 
-curl "$(docker-machine ip swarm-test-1):8080/v1/docker-flow-proxy/reconfigure?serviceName=go-demo&servicePath=/demo&port=8080"
+curl "$(docker-machine ip swarm-test-1):8080/v1/docker-flow-proxy/reconfigure?serviceName=go-demo&servicePath=/demo&port=8080&distribute=true"
