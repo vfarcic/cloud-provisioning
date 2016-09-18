be7a62057ba39e422f560ece0d2371466e297f74
Viktor <viktor@farcic.com>
2016-09-18 17:27:30 -0400

    Jenkins scripts

--- scripts/go-demo.groovy
+++ scripts/go-demo.groovy
@@ -1,91 +1,94 @@
 node("docker") {
 
-    git "https://github.com/vfarcic/go-demo.git"
+  git "https://github.com/vfarcic/go-demo.git"
 
-    stage("Unit") {
+  stage("Unit") {
 
-        sh "docker-compose \
+    sh "docker-compose \
       -f docker-compose-test-local.yml \
       run --rm unit"
 
-        sh "docker-compose \
+    sh "docker-compose \
       -f docker-compose-test-local.yml \
       build app"
 
-    }
+  }
 
-    stage("Staging") {
+  stage("Staging") {
 
-        try {
+    try {
 
-            sh "docker-compose \
+      sh "docker-compose \
         -f docker-compose-test-local.yml \
         up -d staging-dep"
 
-            sh "docker-compose \
+      sh "docker-compose \
         -f docker-compose-test-local.yml \
         run --rm staging"
 
-        } catch(e) {
+    } catch(e) {
 
-            error "Staging failed"
+      error "Staging failed"
 
-        } finally {
+    } finally {
 
-            sh "docker-compose \
+      sh "docker-compose \
         -f docker-compose-test-local.yml \
         down"
 
-        }
-
     }
 
-    stage("Publish") {
+  }
 
-        sh "docker tag go-demo \
+  stage("Publish") {
+
+    sh "docker tag go-demo \
       localhost:5000/go-demo:2.${env.BUILD_NUMBER}"
 
-        sh "docker push \
+    sh "docker push \
       localhost:5000/go-demo:2.${env.BUILD_NUMBER}"
 
-    }
+  }
 
-    stage("Prod-like") {
+  stage("Prod-like") {
 
-        withEnv([
-                "DOCKER_TLS_VERIFY=1",
-                "DOCKER_HOST=tcp://${env.PROD_LIKE_IP}:2376",
-                "DOCKER_CERT_PATH=/machines/${env.PROD_LIKE_NAME}",
-                "HOST_IP=localhost"
-        ]) {
+    withEnv([
+      "DOCKER_TLS_VERIFY=1",
+      "DOCKER_HOST=tcp://${env.PROD_LIKE_IP}:2376",
+      "DOCKER_CERT_PATH=/machines/${env.PROD_LIKE_NAME}",
+      "HOST_IP=localhost"
+    ]) {
 
-            sh "docker service update \
+      sh "docker service update \
         --image localhost:5000/go-demo:2.${env.BUILD_NUMBER} \
         go-demo"
 
-            sh "docker-compose \
+      sh "docker-compose \
         -f docker-compose-test-local.yml \
         run --rm production"
 
-        }
-
     }
 
-    stage("Production") {
-        withEnv([
-                "DOCKER_TLS_VERIFY=1",
-                "DOCKER_HOST=tcp://${env.PROD_IP}:2376",
-                "DOCKER_CERT_PATH=/machines/${env.PROD_NAME}",
-                "HOST_IP=${env.PROD_IP}"
-        ]) {
+  }
 
-            sh "docker service update \
+  stage("Production") {
+    withEnv([
+      "DOCKER_TLS_VERIFY=1",
+      "DOCKER_HOST=tcp://${env.PROD_IP}:2376",
+      "DOCKER_CERT_PATH=/machines/${env.PROD_NAME}",
+      "HOST_IP=${env.PROD_IP}"
+    ]) {
+
+      sh "docker service update \
         --image localhost:5000/go-demo:2.${env.BUILD_NUMBER} \
         go-demo"
 
-            sh "docker-compose \
+      sh "docker-compose \
         -f docker-compose-test-local.yml \
         run --rm production"
-        }
+
     }
+
+  }
+
 }
