4983c3f6fc8deb0f067270c8b6d8145e9534853e
Viktor <viktor@farcic.com>
2016-10-28 18:32:09 +0200

    Prometheus

---
+++ scripts/dm-swarm-5.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+for i in {1..5}; do
+    docker-machine create \
+        -d virtualbox \
+        swarm-$i
+done
+
+eval $(docker-machine env swarm-1)
+
+docker swarm init \
+  --advertise-addr $(docker-machine ip swarm-1)
+
+TOKEN_MANAGER=$(docker swarm join-token -q manager)
+
+TOKEN_WORKER=$(docker swarm join-token -q worker)
+
+for i in 2 3; do
+    eval $(docker-machine env swarm-$i)
+
+    docker swarm join \
+        --token $TOKEN_MANAGER \
+        --advertise-addr $(docker-machine ip swarm-$i) \
+        $(docker-machine ip swarm-1):2377
+done
+
+for i in 4 5; do
+    eval $(docker-machine env swarm-$i)
+
+    docker swarm join \
+        --token $TOKEN_WORKER \
+        --advertise-addr $(docker-machine ip swarm-$i) \
+        $(docker-machine ip swarm-1):2377
+done
+
+for i in {1..5}; do
+    eval $(docker-machine env swarm-1)
+
+    docker node update \
+        --label-add env=prod \
+        swarm-$i
+done
+
+echo ""
+echo ">> The swarm cluster is up and running"
---
+++ scripts/dm-swarm-services-3.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+eval $(docker-machine env swarm-1)
+
+docker network create --driver overlay proxy
+
+docker network create --driver overlay go-demo
+
+docker service create --name proxy \
+    -p 80:80 \
+    -p 443:443 \
+    --network proxy \
+    --replicas 3 \
+    -e MODE=swarm \
+    vfarcic/docker-flow-proxy
+
+docker service create --name swarm-listener \
+    --network proxy \
+    --mount "type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock" \
+    -e DF_NOTIFICATION_URL=http://proxy:8080/v1/docker-flow-proxy/reconfigure \
+    vfarcic/docker-flow-swarm-listener
+
+docker service create --name go-demo-db \
+    --network go-demo \
+    mongo
+
+docker service create --name go-demo \
+    -e DB=go-demo-db \
+    --network go-demo \
+    --network proxy \
+    --replicas 3 \
+    --label com.df.notify=true \
+    --label com.df.distribute=true \
+    --label com.df.servicePath=/demo \
+    --label com.df.port=8080 \
+    vfarcic/go-demo
+
+echo ""
+echo ">> The services scheduled and will be up-and-running soon"
