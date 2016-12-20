4281270fe2ee62c240160d21d329860452a50e22
Viktor <viktor@farcic.com>
2016-12-20 22:15:09 +0100

    AWS

--- scripts/swarm-services-3.sh
+++ scripts/swarm-services-3.sh
@@ -21,5 +21,16 @@ docker service create --name swarm-listener \
     --constraint 'node.role==manager' \
     vfarcic/docker-flow-swarm-listener
 
+docker service create --name jenkins \
+    -e JENKINS_OPTS="--prefix=/jenkins" \
+    --mount "type=volume,source=jenkins,target=/var/jenkins_home,volume-driver=rexray" \
+    --label com.df.notify=true \
+    --label com.df.distribute=true \
+    --label com.df.servicePath=/jenkins \
+    --label com.df.port=8080 \
+    --network proxy \
+    --reserve-memory 300m \
+    jenkins:2.7.4-alpine
+
 echo ""
 echo ">> The scheduled services will be up-and-running soon"
